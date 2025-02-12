c68cdf932ee6f31de0ddbeca1bf9eae8aadbce03
Sambhav Rawani <167787280+Sambhav260904@users.noreply.github.com>
2025-02-12 18:33:31 +0530

    added backend url for server

--- chat-bot/front-end/index2.js
+++ chat-bot/front-end/index2.js
@@ -14,7 +14,7 @@ async function sendMessage() {
     document.getElementById("user-input").value = "";
 
     try {
-        const response = await fetch("http://localhost:5000/chat", {
+        const response = await fetch("https://chat-bot-le2b.onrender.com/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ prompt: userInput }),
